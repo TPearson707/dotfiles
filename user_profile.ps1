3f56f9a8913becd57efc31a2505df8e495cb351b
Thomas Pearson <pearson.e.thomas@gmail.com>
2025-12-14 13:43:01 -0500

    Initial commit

---
+++ user_profile.ps1
@@ -0,0 +1,36 @@
+# Prompt - Git Integration
+Import-Module posh-git
+
+# Load prompt config
+function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
+$PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'thomas.omp.json'
+oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
+
+# Icons
+Import-Module -Name Terminal-Icons
+
+# PSReadLine Auto Complete
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadlineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf Fuzzy Finder 
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# Utilities
+function which ($command) {
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
+        Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+	
